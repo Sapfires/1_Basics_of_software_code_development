17577f5b0eb66305919a7535d1e7d29773dfafaf
Sapfires <nastya-zykov@tut.by>
2021-03-24 23:43:06 +0300

    SubTask17

---
+++ src/com/epam/intro/SubTask17.java
@@ -0,0 +1,10 @@
+package com.epam.intro;
+
+public class SubTask17 {
+    public static void main(String[] args) {
+        for (int c = 0; c < 512; c++) {
+            System.out.println(c + ": " + (char) c);
+
+        }
+    }
+}
